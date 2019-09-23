648d36c80a572aef3e639955b6c2454f134cb394
Andrey <andrey.glushko@ladevelopers.com>
2019-09-23 23:02:04 +0300

    add 'UpdatedAt' to PhraseDto

--- CreoHp.Dto/Phrases/PhraseDto.cs
+++ CreoHp.Dto/Phrases/PhraseDto.cs
@@ -5,5 +5,6 @@ namespace CreoHp.Dto.Phrases
     public sealed class PhraseDto : CreatePhraseDto
     {
         public Guid Id { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
